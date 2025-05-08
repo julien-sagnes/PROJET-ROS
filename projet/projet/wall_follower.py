594fc3c7578307703e609700442856062f7a3ed1
Judicael Corpet <judicael.corpet@gmail.com>
2025-05-08 14:15:03 +0200

    Mise à jour Wall_follower avec kp= 4.2

--- projet/projet/wall_follower.py
+++ projet/projet/wall_follower.py
@@ -30,18 +30,14 @@ class WallFollower(Node):
         self.declare_parameter('linear_speed', 0.05)
         self.linear_speed = self.get_parameter('linear_speed').get_parameter_value().double_value
 
-        # Très grande valeurs arbitraires
+        # Très grandes valeurs arbitraires
         self.front_dist = 10.0
         self.left_dist = 10.0
         self.right_dist = 10.0
 
         # PI
-        self.kp = 1.0
-        self.ki = 0.1
-        self.integral = 0.0
-        self.coef = 1.0
+        self.kp = 4.2
         self.previous_time = time.time()
-        
 
         self.get_logger().info("Wall follower node started.")
 
@@ -60,45 +56,32 @@ class WallFollower(Node):
         if abs(now - self.start_time) < self.wait_time:
             self.get_logger().info(f"Attente durant {now - self.start_time} / {self.wait_time}.")
             return
-        
-        # Retour en ligne droite (réinitialisation du PID progressif)
-        if self.front_dist > 0.8 or math.isinf(self.front_dist):
-            self.coef -= 0.008  # à modifier avec test sur robot réel
-            self.get_logger().warn("Détection de ligne droite.")
-            self.integral *= self.coef
 
         # Mur perdu : avance lentement
-        correction_add = 0.05
-        if abs(self.right_dist) > 0.3 or math.isinf(self.right_dist):
-            self.get_logger().warn("Mur perdu !")
+        tolerance = 0.3
+        correc_add = 0.05
+        if abs(self.right_dist) > tolerance or math.isinf(self.right_dist):
+            self.get_logger().warn("Mur droit perdu !")
             twist.linear.x = self.linear_speed
-            twist.angular.z = - correction_add
-        elif abs(self.left_dist) > 0.3 or math.isinf(self.left_dist):
-            self.get_logger().warn("Mur perdu !")
+            twist.angular.z = - correc_add
+        elif abs(self.left_dist) > tolerance or math.isinf(self.left_dist):
+            self.get_logger().warn("Mur gauche perdu !")
             twist.linear.x = self.linear_speed
-            twist.angular.z = + correction_add
+            twist.angular.z = + correc_add
+
 
         else:
             self.coef = 1.0
 
             error = self.left_dist - self.right_dist
 
-            # Calcul du temps écoulé
-            current_time = time.time()
-            dt = current_time - self.previous_time
-            self.previous_time = current_time
-
-            # Mise à jour de l'intégrale
-            self.integral += error * dt
-
-            correction = self.kp * error + self.ki * self.integral
-
+            correction = self.kp * error
             twist.linear.x = self.linear_speed
             twist.angular.z = correction
 
             self.get_logger().info(
             f"Front: {self.front_dist:.2f}, Right: {self.right_dist:.3f}, Left: {self.left_dist}, "
-            f"Proportionnal: {self.kp * error:.3f}, Integral: {(self.ki * self.integral):.3f}, Angular Z: {twist.angular.z:.3f}")
+            f"Proportionnal: {self.kp * error:.3f}, Angular Z: {twist.angular.z:.3f}")
 
         self.publisher.publish(twist)
 
